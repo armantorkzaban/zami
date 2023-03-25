34c34e00277022688ca62676333d8501546c3356
Arman Torkzaban <armantorkzaban@icloud.com>
2023-03-25 19:46:55 +0100

    Moving on setting up the env.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:zemin/pages/custom_tile_page.dart';
+import 'package:zemin/pages/interactive_page.dart';
+import 'package:zemin/pages/markers_page.dart';
+import 'package:zemin/pages/metro_lines_page.dart';
+import 'package:zemin/pages/raster_map_page.dart';
+import 'package:zemin/pages/shapes_page.dart';
+import 'package:zemin/pages/twilight_page.dart';
+import 'package:zemin/pages/vector_map_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MapApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MapApp extends StatelessWidget {
+  const MapApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Map Examples',
+      supportedLocales: const [Locale('en')],
+      localizationsDelegates: const [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorSchemeSeed: Colors.purple,
         useMaterial3: true,
+        brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Map Examples'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Raster Map'),
+            subtitle: const Text('Raster tiles from Google, OSM and etc.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const RasterMapPage()),
+          ),
+          ListTile(
+            title: const Text('Vector Map'),
+            subtitle: const Text('OSM light-themed vector maps.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const VectorMapPage()),
+            enabled: false,
+          ),
+          ListTile(
+            title: const Text('Markers'),
+            subtitle: const Text(
+                'Drop multiple fixed and centered markers on the map.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const MarkersPage()),
+          ),
+          ListTile(
+            title: const Text('Interactive'),
+            subtitle: const Text('Say where on the earth user has clicked.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const InteractiveMapPage()),
+          ),
+          ListTile(
+            title: const Text('Shapes'),
+            subtitle: const Text('Display Polylines on the map.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const ShapesPage()),
+          ),
+          ListTile(
+            title: const Text('Custom Tiles'),
+            subtitle: const Text('Use any Widget as map tiles.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const CustomTilePage()),
+          ),
+          ListTile(
+            title: const Text('Metro Lines (Work in Progress)'),
+            subtitle: const Text('Draw polyline overlays (Tehran Metro).'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const MetroLinesPage()),
+          ),
+          ListTile(
+            title: const Text('Custom Projection'),
+            subtitle:
+                const Text('How we convert LatLng to XY. Useful for games.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: _showNotImplemented,
+            enabled: false,
+          ),
+          ListTile(
+            title: const Text('Twilight'),
+            subtitle: const Text('Day and night map, sun and moon position.'),
+            trailing: const Icon(Icons.chevron_right_sharp),
+            onTap: () => _push(const TwilightPage()),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _showNotImplemented() {
+    const snackBar =
+        SnackBar(content: Text('This demo is not implemented yet.'));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void _push(Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
     );
   }
 }
